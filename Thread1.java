dcebdf8ffedfa6ff74d56873c448b65fec015e77
xueQaQ <xuebaigao@163.com>
2018-05-15 23:17:56 +0800

    thread1

---
+++ Thread1.java
@@ -0,0 +1,32 @@
+package javaThread;
+
+/*
+ *继承Thread类实现多线程 
+ */
+
+//class Mythread extends Thread{
+//	private String title;
+//	public Mythread(String title) {
+//	this.title = title;
+//}
+//
+//public void run() {
+//	for(int i = 0; i < 10; i++) {
+//		System.out.println(this.title+"i = "+i);
+//		}
+//	}
+//}
+//
+//public class Thread1 {
+//	public static void main(String[] args) {
+//		Mythread mythread1 = new Mythread("thread1");
+//		Mythread mythread2 = new Mythread("thread2");
+//		Mythread mythread3 = new Mythread("thread3");
+////		mythread1.run();
+////		mythread2.run();
+////		mythread3.run();
+//		mythread1.start();
+//		mythread2.start();
+//		mythread3.start();
+//	}		
+//}
